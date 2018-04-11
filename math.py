33290cf487a51ae189c716bcad69b046688b9896
Peijun Zhu <zpj.ustc@gmail.com>
2018-04-10 22:57:48 -0400

    Use latest mathjax

--- math.py
+++ math.py
@@ -32,7 +32,7 @@ class Mathjax:
 }
 </script>
 """
-    cdn = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js'
+    cdn = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/latest.js'
     cached = False
 
     @staticmethod
